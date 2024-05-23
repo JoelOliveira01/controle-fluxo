12b85592f97c05ad6d814b86bacfb88e57357f51
Joel Oliveira <joeljoseromero7@gmail.com>
2024-05-23 09:53:02 -0300

    .

--- src/pacote-condicionais/PlanoOperadora.java
+++ src/pacote-condicionais/PlanoOperadora.java
@@ -1,3 +1,4 @@
+
 public class PlanoOperadora {
     public static void main(String[] args) {
         String  plano = "M";
--- src/pacote-condicionais/SistemaMedida.java
+++ src/pacote-condicionais/SistemaMedida.java
@@ -1,4 +1,3 @@
-
 public class SistemaMedida {
     public static void main(String[] args) {
         
